8e2dd0c1df82fb5bd87a09d431d532a1b7b36e36
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2022-10-04 13:43:43 +0200

    dwdw

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -25,6 +25,10 @@ export default function Document() {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
       </Head>
       <body className="text-sm bg-dark-500 text-white tracking-[1px]">
         <Main />
