5e8aad4c483fd7632ce4fe3f8cdae50f25098a84
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2022-10-04 13:12:14 +0200

    fix errors

--- src/components/navbar-desktop.js
+++ src/components/navbar-desktop.js
@@ -106,7 +106,7 @@ export default function NavbarDesktop({ ...restProps }) {
 
   useEffect(
     () => setIsNavbarOpened(JSON.parse(getItem("navbar-opened"))),
-    [isNavbarOpened]
+    [isNavbarOpened, getItem]
   );
 
   return (
