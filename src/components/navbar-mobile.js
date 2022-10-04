38653d5ab8cc55cb674f5915cd98685d1845c45d
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2022-10-04 13:33:42 +0200

    added header

--- src/components/navbar-mobile.js
+++ src/components/navbar-mobile.js
@@ -15,6 +15,7 @@ import {
 } from "../lib/icons";
 import { useRouter } from "next/router";
 import SimpleBar from "simplebar-react";
+import Header from "./header";
 import "simplebar-react/dist/simplebar.min.css";
 
 const navArray = [
@@ -84,82 +85,86 @@ export default function NavbarMobile({ closeMenu, ...restProps }) {
   const router = useRouter();
 
   return (
-    <div
-      className="fixed left-0 top-[68px] h-[calc(100%_-_68px)] bg-dark-400 w-full z-50"
-      {...restProps}
-    >
-      <div className="px-4 py-[14px] flex items-center gap-16 bg-dark-600 mb-2">
-        <form className="relative grow">
-          <Search className="w-5 h-5 absolute left-2 top-1/2 -translate-y-1/2 text-dark-300" />
-          <input
-            placeholder="Search.."
-            className="h-10 w-full bg-transparent rounded border border-green-500 py-2 pr-3 pl-10 ring-0 focus:ring-0 focus:border-green-300 focus:outline-none placeholder:text-sm placeholder:tracking-[1px] tracking-[1px] placeholder:text-dark-300 text-white font-sans"
-          />
-        </form>
+    <>
+      <div
+        className="fixed left-0 top-0 h-full bg-dark-400 w-full z-50"
+        {...restProps}
+      >
+        <Header />
 
-        <button
-          type="button"
-          className="flex-shrink-0 text-white"
-          onClick={closeMenu}
-        >
-          <MenuClose className="w-6 h-6" />
-        </button>
-      </div>
+        <div className="px-4 py-[14px] flex items-center gap-16 bg-dark-600 mb-2">
+          <form className="relative grow">
+            <Search className="w-5 h-5 absolute left-2 top-1/2 -translate-y-1/2 text-dark-300" />
+            <input
+              placeholder="Search.."
+              className="h-10 w-full bg-transparent rounded border border-green-500 py-2 pr-3 pl-10 ring-0 focus:ring-0 focus:border-green-300 focus:outline-none placeholder:text-sm placeholder:tracking-[1px] tracking-[1px] placeholder:text-dark-300 text-white font-sans"
+            />
+          </form>
 
-      <SimpleBar style={{ maxHeight: "calc(100vh - 220px)", width: "100%" }}>
-        <div className="space-y-4">
-          <nav className="flex flex-col gap-1">
-            {navArray.map((item, index) => {
-              return (
-                <Link href={item.path} key={index}>
-                  <a
-                    className={`${
-                      router.pathname === item.path
-                        ? "text-green-500 bg-dark-700"
-                        : "text-white"
-                    } hover:text-green-500 py-2 px-[12px] rounded text-base flex items-center gap-[12px] transition`}
-                  >
-                    {item.icon}
-                    {item.title}
-                  </a>
-                </Link>
-              );
-            })}
-          </nav>
-
-          <div className="h-[1px] w-full bg-[#213137]"></div>
-
-          <nav className="flex flex-col gap-1 [&>*]:py-[10px] [&>*]:pl-12 [&>*]:pr-4 [&>*]:transition">
-            {secondNavArray.map((item, index) => {
-              return (
-                <Link href={item.path} key={index}>
-                  <a>{item.title}</a>
-                </Link>
-              );
-            })}
-            <Link href="/buy-crypto">
-              <a className="font-heading !text-green-500">Buy crypto</a>
-            </Link>
-          </nav>
-
-          <div className="h-[1px] w-full bg-[#213137]"></div>
-
-          <nav className="flex flex-col gap-1 [&>*]:py-2 [&>*]:px-[12px] [&>*]:rounded [&>*]:text-base [&>*]:flex [&>*]:items-center [&>*]:gap-[12px] [&>*]:transition">
-            <Link href="/support">
-              <a
-                className={`${
-                  router.pathname === "/support"
-                    ? "text-green-500 bg-dark-700"
-                    : "text-white"
-                } hover:text-green-500`}
-              >
-                <Support className="flex-shrink-0 w-6 h-6" />
-                Support
-              </a>
-            </Link>
-          </nav>
+          <button
+            type="button"
+            className="flex-shrink-0 text-white"
+            onClick={closeMenu}
+          >
+            <MenuClose className="w-6 h-6" />
+          </button>
         </div>
-      </SimpleBar>
-    </div>
+
+        <SimpleBar style={{ maxHeight: "calc(100vh - 160px)", width: "100%" }}>
+          <div className="space-y-4">
+            <nav className="flex flex-col gap-1">
+              {navArray.map((item, index) => {
+                return (
+                  <Link href={item.path} key={index}>
+                    <a
+                      className={`${
+                        router.pathname === item.path
+                          ? "text-green-500 bg-dark-700"
+                          : "text-white"
+                      } hover:text-green-500 py-2 px-[12px] rounded text-base flex items-center gap-[12px] transition`}
+                    >
+                      {item.icon}
+                      {item.title}
+                    </a>
+                  </Link>
+                );
+              })}
+            </nav>
+
+            <div className="h-[1px] w-full bg-[#213137]"></div>
+
+            <nav className="flex flex-col gap-1 [&>*]:py-[10px] [&>*]:pl-12 [&>*]:pr-4 [&>*]:transition">
+              {secondNavArray.map((item, index) => {
+                return (
+                  <Link href={item.path} key={index}>
+                    <a>{item.title}</a>
+                  </Link>
+                );
+              })}
+              <Link href="/buy-crypto">
+                <a className="font-heading !text-green-500">Buy crypto</a>
+              </Link>
+            </nav>
+
+            <div className="h-[1px] w-full bg-[#213137]"></div>
+
+            <nav className="flex flex-col gap-1 [&>*]:py-2 [&>*]:px-[12px] [&>*]:rounded [&>*]:text-base [&>*]:flex [&>*]:items-center [&>*]:gap-[12px] [&>*]:transition">
+              <Link href="/support">
+                <a
+                  className={`${
+                    router.pathname === "/support"
+                      ? "text-green-500 bg-dark-700"
+                      : "text-white"
+                  } hover:text-green-500`}
+                >
+                  <Support className="flex-shrink-0 w-6 h-6" />
+                  Support
+                </a>
+              </Link>
+            </nav>
+          </div>
+        </SimpleBar>
+      </div>
+    </>
   );
 }
